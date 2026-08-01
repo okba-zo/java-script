5174b0cd6f512653205818fe8e04a1140a4927cb
okba <Calisthenicsokba@gmail.com>
2026-08-01 11:35:10 +0100

    learned variables

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-hello world
+let name = "okba zf";
+let age = 22;
+let student = false;
+
+document.getElementById("p1").textContent = "your name is " + name;
+document.getElementById("p2").textContent = "you are " + age + " years old";
+document.getElementById("p3").textContent = "you are a student: " + student;
