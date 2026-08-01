63d03643a54b1cf05ed0bd69e1dfd964de71c2a9
okba <Calisthenicsokba@gmail.com>
2026-08-01 12:06:45 +0100

    user input with window prompt

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-
-let students = 30;
-students += 5;
-students -= 2;
-students *=2;
-students **= 2;
-let extra = students % 2;
-console.log("Number of students:", students);
-console.log("Extra students:", extra);
+let username;
+username = window.prompt("Please enter your username:");
+console.log("Username entered:", username);
